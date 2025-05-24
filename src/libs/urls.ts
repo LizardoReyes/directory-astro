095e049545b07de782b10351a2d33ac10c7e8533
Lizardo Reyes Jara <ciberutilidades@gmail.com>
2025-05-24 11:32:28 -0500

    cambios en las rutas permanlink

--- src/libs/urls.ts
+++ src/libs/urls.ts
@@ -3,24 +3,22 @@ import type {CategoryType} from "../types/CategoryType.ts";
 import {getSiteInfo} from "./csv.ts";
 import {createSlug} from "./global.ts";
 
-const { baseUrl } = getSiteInfo();
-
 export function createPostUrl(post: PostType | undefined, category: CategoryType | undefined): string {
     if (!post || !category) return "";
     const categorySlug = category.slug || createSlug(category.title)
     const postSlug = post.slug || createSlug(post.title)
-    return `${baseUrl}/${categorySlug}/${postSlug}`;
+    return `/${categorySlug}/${postSlug}`;
 }
 
 export function createCategoryUrl(category: CategoryType | undefined): string {
     if (!category) return "";
     const categorySlug = category.slug || createSlug(category.title)
-    return `${baseUrl}/${categorySlug}`;
+    return `/${categorySlug}`;
 }
 
 export function createPageUrl(page: string | undefined): string {
     if (!page) return "";
-    return `${baseUrl}/${page}`;
+    return `/${page}`;
 }
 
 export function formatContent(content: string | undefined): string {
